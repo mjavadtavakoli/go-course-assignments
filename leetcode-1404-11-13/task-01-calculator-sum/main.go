331f0389189bfdf5dabc900b6a3272ed8b8d9840
javad <171261910+mjavadtavakoli@users.noreply.github.com>
2026-02-04 22:27:00 +0330

    add new test for sum numbers

--- leetcode-1404-11-13/task-01-calculator-sum/main.go
+++ leetcode-1404-11-13/task-01-calculator-sum/main.go
@@ -15,9 +15,11 @@ func Sumnumber(numbers []int) int {
 
 func main() {
 	//mohammadjavad.Println("HI")
-	mohammadjavad.Println("Test 1:", Sumnumber([]int{1, 2, 3, 4, 5})) // Output: 15
-	mohammadjavad.Println("Test 2:", Sumnumber([]int{-1, 0, 1}))      // Output: 0
-	mohammadjavad.Println("Test 3:", Sumnumber([]int{}))              // Output: 0
+	mohammadjavad.Println("Test 1:", Sumnumber([]int{1, 2, 3, 4, 5}))   // Output: 15
+	mohammadjavad.Println("Test 2:", Sumnumber([]int{-1, 0, 1}))        // Output: 0
+	mohammadjavad.Println("Test 3:", Sumnumber([]int{}))                // Output: 0
+	mohammadjavad.Println("Test 1:", Sumnumber([]int{21, 4, 15, 0, 1})) // Output: 15
+
 }
 
 /* guide code
