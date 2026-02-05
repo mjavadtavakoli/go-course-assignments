0f8384c09120168f8fb97767e41a7a05d44f3857
javad <171261910+mjavadtavakoli@users.noreply.github.com>
2026-02-05 22:24:28 +0330

    create func valid ispalindrome

---
+++ chapter5-section6/challenge-02-Valid Palindrome/main.go
@@ -0,0 +1,26 @@
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+
+	left := 0
+	right := len(s) - 1
+
+	for left < right {
+
+		for left < right && !unicode.IsLetter(rune(s[left])) && !unicode.IsDigit(rune(s[left])) {
+			left++
+		}
+
+		for left < right && !unicode.IsLetter(rune(s[right])) && !unicode.IsDigit(rune(s[right])) {
+			right--
+		}
+
+		if s[left] != s[right] {
+			return false
+		}
+
+		left++
+		right--
+	}
+
+	return true
+}
