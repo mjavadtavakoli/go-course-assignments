a5b7e3f79941fcef33f0b5baf5556471f8273582
javad <171261910+mjavadtavakoli@users.noreply.github.com>
2026-02-08 20:10:13 +0330

    fix challenge for daily learn

--- test-local/taskONE.go
+++ test-local/taskONE.go
@@ -6,21 +6,3 @@ can add a new student
 
 and finally prints all the students
 */
-
-package main
-
-import "fmt"
-
-func main() {
-
-	// لیست اولیه دانشجوها
-	students := []string{"Ali", "Reza", "Sara"}
-
-	// اضافه کردن دانشجوی جدید
-	students = append(students, "Mohammad")
-
-	// چاپ همه دانشجوها
-	for i, name := range students {
-		fmt.Println(i+1, name)
-	}
-}
--- test-local/taskTwo.go
+++ test-local/taskTwo.go
@@ -5,4 +5,3 @@ Stores the name and quantity of a product
 If the product exists, decrements its quantity
 
 Prints the ending inventory*/
-
