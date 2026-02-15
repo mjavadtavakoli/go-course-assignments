c0813da39620cc73f57884e16ac1526c94f31fd0
mohammad javad <tvklqa@gmail.com>
2026-02-15 16:16:48 +0330

    intial unit test folder for try and learn

---
+++ test-local/learn-tdd-test/main.go
@@ -0,0 +1,6 @@
+package main
+
+// Sum دو عدد را گرفته و مجموع آن‌ها را برمی‌گرداند
+func Sum(a int, b int) int {
+	return a + b
+}
---
+++ test-local/learn-tdd-test/main_Test.go
@@ -0,0 +1,17 @@
+//  این فایل برای یونیت تست نوشتن است
+
+package main
+
+import (
+	"testing"
+)
+
+type TestCase struct {
+	a              int
+	b              int
+	expectedresult int
+}
+
+func TestSum(t *testing.T) {
+
+}
